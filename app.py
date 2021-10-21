554ee8fb4393f0eb4cbe37adbf8be6c4b29653f9
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-10-21 14:18:08 +0530

    fix

--- app.py
+++ app.py
@@ -10,23 +10,24 @@ from scipy.stats import kurtosis, skew
 from scipy import ndimage
 import skimage.measure
 import pandas as pd
-  
+
 app = Flask(__name__)
-cors = CORS(app)  
+cors = CORS(app)
 
-model=pickle.load(open('c:/Users/adhyansh/Desktop/web demo/final_Demo/final/api/model.pkl','rb'))
+model = pickle.load(open('./model.pkl', 'rb'))
 
-@app.after_request
 
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     return json.dumps("predict")
+
 
 @app.route('/', methods=['GET'])
 def helloworld():
     if(request.method == 'GET'):
-        
-        return json.dumps("hello")  
-  
+
+        return json.dumps("hello")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
